27cccae14690203c8a581aaa3bc4d9cd3ea0a5da
Elijah Haga <HagaElijah04@gmail.com>
2025-02-12 22:35:50 -0500

    Added back the last change as a comment
    This way if I want to re-add it in the future it'll be very easy

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const observer = new IntersectionObserver((entries) => {
         if(entry.isIntersecting) {
             entry.target.classList.add('show');
         }
+/*      else {
+            entry.target.classList.remove('show');
+        }
+*/
     });
 },  { threshold: 0.2 });
 
