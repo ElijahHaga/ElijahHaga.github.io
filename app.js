7bf47252d3354930cd24bef309acf7cb4eb9219f
Elijah Haga <HagaElijah04@gmail.com>
2025-02-12 22:52:27 -0500

    Fixing js code

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-/*      else {
-            entry.target.classList.remove('show');
-        }
-*/
-    });
-},  { threshold: 0.2 });
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
-
 document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if(entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+    /*      else {
+                entry.target.classList.remove('show');
+            }
+    */
+        });
+    },  { threshold: 0.2 });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+
     ["carousel-led-ring", "carousel-raspberry-pi-pico", "carousel-gameboy-color-cartridge", "carousel-this-page"].forEach(id => {
         new Splide(`#${id}`, {
             perPage: 2,
             breakpoints: { 640: { perPage: 1 } },
         }).mount();
     });
+
+    document.getElementById("toggleButton").addEventListener("click", function () {
+        toggleIframe();
+    });
 });
+
 
 function toggleIframe() { /* The function to toggle the Li-ion battery paper */
     const iframeContainer = document.getElementById("iframeContainer");
